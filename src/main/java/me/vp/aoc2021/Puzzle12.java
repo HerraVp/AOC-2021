e45625330ff4197522c4800748c440687c4bfe5e
Vp <vp2b2t@gmail.com>
2021-12-13 17:12:04 +0200

    Day 11-13

---
+++ src/main/java/me/vp/aoc2021/Puzzle12.java
@@ -0,0 +1,56 @@
+package me.vp.aoc2021;
+
+import java.util.*;
+
+public class Puzzle12 extends AbstractPuzzle {
+    private final Map<String, Set<String>> edges = new HashMap<>();
+
+    public Puzzle12(String puzzleInput) {
+        super(puzzleInput);
+        for (var line : getPuzzleInput().split("\n")) {
+            var parts = line.split("-");
+            var from = parts[0];
+            var to = parts[1];
+            edges.computeIfAbsent(from, s -> new HashSet<>()).add(to);
+            edges.computeIfAbsent(to, s -> new HashSet<>()).add(from);
+        }
+    }
+
+    @Override
+    public int getDay() {
+        return 12;
+    }
+
+    @Override
+    public String solvePart1() {
+        return String.valueOf(countPaths("start", new LinkedList<>(), true));
+    }
+
+    @Override
+    public String solvePart2() {
+        return String.valueOf(countPaths("start", new LinkedList<>(), false));
+    }
+
+    private int countPaths(String cave, LinkedList<String> path, boolean seenTwice) {
+        if (cave.equals("end")) {
+            return 1;
+        }
+        if (isSmallCave(cave) && path.contains(cave)) {
+            if (seenTwice || cave.equals("start")) {
+                return 0;
+            }
+            seenTwice = true;
+        }
+        path.addLast(cave);
+        var count = 0;
+        for (var next : edges.get(cave)) {
+            count += countPaths(next, path, seenTwice);
+        }
+        path.removeLast();
+        return count;
+    }
+
+    private static boolean isSmallCave(String cave) {
+        return cave.charAt(0) >= 'a';
+    }
+}
---
+++ src/main/java/me/vp/aoc2021/ocr/OcrException.java
@@ -0,0 +1,11 @@
+package me.vp.aoc2021.ocr;
+
+public class OcrException extends Exception {
+    public OcrException(String message) {
+        super(message);
+    }
+
+    public OcrException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
